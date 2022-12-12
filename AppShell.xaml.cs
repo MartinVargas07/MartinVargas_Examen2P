b77d73278310eafc4d4580723e724803b8f85046
laboratorio <laboratorio@UPOAULA10224>
2022-12-12 07:20:11 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MartinVargas_Examen2P;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MartinVargas_Examen2P;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
